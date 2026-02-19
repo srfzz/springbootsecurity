17db5e761fc1ddf83bc67a4134eef031ccaba90e
Sarfaraj Ansari <imsarfaraaj7@gmail.com>
2026-02-19 10:54:28 +0530

    added a few  lines of code where i have disabled the csrf token and in session make it satess eans it wont e saving ansy sessio insie my redis or db or file

--- src/main/java/com/posts/demo/config/WebSecurityConfig.java
+++ src/main/java/com/posts/demo/config/WebSecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
@@ -14,7 +16,11 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 public class WebSecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
-   http.authorizeHttpRequests(auth-> auth.requestMatchers("/posts/**").permitAll().anyRequest().authenticated()).formLogin(Customizer.withDefaults());
+   http
+           .authorizeHttpRequests(auth-> auth.requestMatchers("/posts/**").permitAll().anyRequest().authenticated())
+           .csrf(AbstractHttpConfigurer::disable)
+           .sessionManagement(sessionManagementConfig->sessionManagementConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+           //.formLogin(Customizer.withDefaults());
    return http.build();
     }
 }
